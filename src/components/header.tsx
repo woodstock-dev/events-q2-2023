b999eddfe183754b0789f4d76eed0b94436ee536
Josilyn McGuinness <josilyn_butler@yahoo.com>
2023-07-09 22:46:37 -0400

    Styling - getting aquainted with MUI

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return(
     <div className="header">
-      <h1>MaP app</h1>
-      <h4 className="small-heading">Make a Plan</h4>
+      <h1>PI</h1>
+      <h4 className="small-heading">Plan It</h4>
       <nav>
         <Link className="nav-links" to="/">Login</Link>
         <Link className="nav-links" to="/eventList">Event List</Link>
--- src/pages/addEvents.tsx
+++ src/pages/addEvents.tsx
@@ -1,19 +1,57 @@
 import './addEvents.css'
+import Button from '@mui/material/Button';
+//import Card from '@mui/material/Card';
+//import CardActions from '@mui/material/CardActions';
+//import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { LocalizationProvider, TimePicker } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import TextField from '@mui/material/TextField';
+import TextareaAutosize from '@mui/base/TextareaAutosize';
+//import { ReactNode } from 'react';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+
 
 const AddEvents = () => {
   return(
-    <div className="event-form">
-      <h2 className="add-event-text">Add an Event</h2>
-      <form>
-        <input className="event-input" type="date" aria-label="date" placeholder="dd/mm/yyyy" />
-        <input className="event-input" type="time" aria-label="time" placeholder="hh:mm" />
-        <input className="event-input" type="text" aria-label="title" placeholder="Event Title" />
-        <input className="event-input" type="text" aria-label="location" placeholder="Location" />
-        <textarea className="event-input, text-area" aria-label="notes" placeholder="Notes" />
-        <input type="submit" value="Submit" name="submit" />
-      </form>
-    </div>
+      <Paper className="paper" elevation={3}>
+        <Grid container direction="row" justifyContent="space-between" spacing={1}>
+          <Grid item xs={12}>
+            <Typography variant="h5" align="center" className="add-event-text">Create a Plan</Typography>
+          </Grid>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <Grid item xs={6}>
+            <TimePicker className=" input-item" label="Time" />
+          </Grid>
+          <Grid item xs={6}>
+            <DatePicker className=" input-item" label="Date" />
+          </Grid>
+            <Grid item xs={12}>
+              <TextField className="input-item" fullWidth variant="outlined" label="Event" type="text" required/>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField className="input-item" fullWidth variant="outlined" label="Location" type="text" />
+            </Grid>
+            <Grid item xs={12}>
+              <TextareaAutosize className="input-item" style={{ width: "100%" }} minRows="7" placeholder="Notes"/> 
+            </Grid>
+          </LocalizationProvider>
+          <Grid item xs={3}>
+            <Button className="submit" variant="contained">Add</Button>
+          </Grid>
+        </Grid>
+      </Paper>
   )
 }
 
 export default AddEvents;
+
+/*const MyCard = ({children}:{children:ReactNode}) => {
+  return(
+    <Card className="card">
+      {children}
+    </Card>
+  )
+}*/
