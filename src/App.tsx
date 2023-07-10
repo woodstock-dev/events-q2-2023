8c7e297c7bb21a00a568784a91afbdf7149c6bb3
JordanD <jjosephd804@gmail.com>
2023-07-09 21:59:51 -0400

    TimePicker component used for users to select a time

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import AddEvents from './pages/addEvents'
 import Header from './components/header'
 import Footer from './components/footer'
 
+/**Localization provider and adapter for date component library */
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 import { ReactDOM } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -11,9 +14,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
     <Router>
-
-      <Header/>
+      
+        <Header />
       <Routes>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/addEvents' element={<AddEvents/>}></Route>
@@ -21,6 +25,7 @@ function App() {
       <Footer />
       
    </Router>
+   </LocalizationProvider>
   )
 }
 
--- src/pages/addEvents.tsx
+++ src/pages/addEvents.tsx
@@ -1,23 +1,48 @@
 // Copyright 2023 YOUR NAME HERE
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+import {
+  DatePicker,
+  TimePicker,
+} from '@mui/x-date-pickers';
+import dayjs from 'dayjs';
+import { Box } from '@mui/system';
+import { TextField, Container } from '@mui/material';
 
 const AddEvents = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      {/**
+       * Using MUI box component to create proper spacing for form
+       */}
+      <Box
+        sx={{
+          display: 'flex',
+          mt: 20,
 
-export default AddEvents
+          justifyContent: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <DatePicker />
+        <TimePicker
+          label='Select a Time'
+          defaultValue={dayjs('2022-04-17T15:30')}
+        />
+        <TextField label='Event Description' variant='outlined' />
+      </Box>
+    </div>
+  );
+};
+
+export default AddEvents;
