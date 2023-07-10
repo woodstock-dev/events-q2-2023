e3b285f19f29ade74c805025c7838caef407ad09
JordanD <jjosephd804@gmail.com>
2023-07-09 20:00:45 -0400

    Initial commit featuring login page and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import './App.css'
+
+import Login from './pages/login'
+import AddEvents from './pages/addEvents'
+import Header from './components/header'
+import Footer from './components/footer'
+
+
+import { ReactDOM } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-   <>Empty App</>
+    <Router>
+
+      <Header/>
+      <Routes>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/addEvents' element={<AddEvents/>}></Route>
+      </Routes>
+      <Footer />
+      
+   </Router>
   )
 }
 
