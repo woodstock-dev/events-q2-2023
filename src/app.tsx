cb40d2c5b8592a33c3ef26fea10640f2776674f4
Ryan McGuinness <ryan@fallingonline.com>
2023-07-12 17:16:28 -0400

    Grrrr

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,27 @@
-import { Outlet, Route, Routes } from "react-router-dom";
-import "./app.css";
-import Login from "./pages/login";
-import Events from "./pages/events";
-import RequireAuth from "./components/requireAuth";
-import Header from "./components/header";
-import Footer from "./components/footer";
-import React from "react";
+import { Outlet, Route, Routes } from 'react-router-dom'
+import './app.css'
+import Login from './pages/login'
+import Events from './pages/events'
+import RequireAuth from './components/requireAuth'
+import Header from './components/header'
+import Footer from './components/footer'
+import EventForm from './pages/eventForm'
+import React from 'react'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Login />} />
-        <Route
-          path="events"
-          element={
-            <RequireAuth>
-              <Events />
-            </RequireAuth>
-          }
-        />
+          <Route path="events" element={
+            <RequireAuth><Events /></RequireAuth>
+          } />
+          <Route path="events/create" element={
+            <RequireAuth><EventForm /></RequireAuth>
+          } />
+          <Route path="events/{id}" element={
+            <RequireAuth><EventForm /></RequireAuth>
+          } />
       </Route>
     </Routes>
   );
