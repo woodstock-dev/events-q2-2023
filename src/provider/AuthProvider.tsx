8f55fa5dc399044005deb99958ea88fc47162cfe
Ryan McGuinness <ryan@fallingonline.com>
2023-07-13 19:38:29 -0400

    Update

--- src/provider/AuthProvider.tsx
+++ src/provider/AuthProvider.tsx
@@ -1,20 +1,6 @@
 import React from "react";
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-export const AuthContext = React.createContext<AuthContextType>(null!);
-
-const fakeAuthProvider = {
-  isAuthenticated: false,
-  signin(callback: VoidFunction) {
-    fakeAuthProvider.isAuthenticated = true;
-    setTimeout(callback, 100); // fake async
-  },
-  signout(callback: VoidFunction) {
-    fakeAuthProvider.isAuthenticated = false;
-    setTimeout(callback, 100);
-  },
-};
-
+// An interface describing the authentication context type.
 export interface AuthContextType {
   user: string | null;
   isAuthenticated: () => boolean;
@@ -22,6 +8,34 @@ export interface AuthContextType {
   signout: (callback: VoidFunction) => void;
 }
 
+// Create the Auth Context at the top level of the code
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+export const AuthContext = React.createContext<AuthContextType>(null!);
+
+// This is a simple fake authentication provider
+const fakeAuthProvider = {
+  isAuthenticated: false,
+
+  // Creates a default signin function
+  signin(callback: VoidFunction) {
+    // Sets the authenticated value to true
+    fakeAuthProvider.isAuthenticated = true;
+    // Creates a fake lag time you may find in a normal login
+    setTimeout(callback, 100); // fake async
+  },
+
+  // Creates a fake signout function
+  signout(callback: VoidFunction) {
+    // sets the authentication value to false
+    fakeAuthProvider.isAuthenticated = false;
+
+    // Creates a fake lag time similar to normal log outs
+    setTimeout(callback, 100);
+  },
+};
+
+// The actual authentication provider, it's a components
+// and therefore can store state
 function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = React.useState<string | null>(null);
 
@@ -43,7 +57,12 @@ function AuthProvider({ children }: { children: React.ReactNode }) {
     });
   };
 
-  const value = {user: user, signin: signin, signout: signout, isAuthenticated: isAuthenticated} as AuthContextType
+  const value = {
+    user: user, 
+    signin: signin, 
+    signout: signout, 
+    isAuthenticated: isAuthenticated
+  } as AuthContextType
 
   return(
     <AuthContext.Provider value={value}>
